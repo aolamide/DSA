9499054f68aca605026f8e345add8c31001e100d
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-07-26 23:09:21 +0100

    Time: 3 ms (100.00%), Space: 20.9 MB (76.34%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.php
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxDepth($root, $depth = 0) {
+        if(!$root) return $depth;
+        $depth++;
+        
+        return max($this->maxDepth($root->left, $depth), $this->maxDepth($root->right, $depth));
+    }
+}
