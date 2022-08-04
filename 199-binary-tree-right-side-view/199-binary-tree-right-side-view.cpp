7be774877383aa25b9a9f3ac54bd7961e1aecd16
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-08-04 19:49:59 +0100

    Time: 3 ms (85.34%), Space: 12.5 MB (5.53%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -9,34 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//BFS Solution
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root) return {};
+        
+//         queue<TreeNode*> q;
+//         vector<int> result;
+
+//         int levelCount = 1;
+//         int count = 0;
+
+//         q.push(root);
+
+//         while(!q.empty()) {
+//             root = q.front();
+//             q.pop();
+//             count++;
+
+//             if(root->left) q.push(root->left);
+//             if(root->right) q.push(root->right);
+
+//             if(count == levelCount) {
+//                 result.push_back(root->val);
+//                 levelCount = q.size();
+//                 count = 0;
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
+
+
+//DFS Solution
 class Solution {
 public:
+    vector<int> result;
+    set<int> levelSet;
     vector<int> rightSideView(TreeNode* root) {
-        if(!root) return {};
+        int level = 0;
         
-        queue<TreeNode*> q;
-        vector<int> result;
+        preorder(root, level);
 
-        int levelCount = 1;
-        int count = 0;
-
-        q.push(root);
-
-        while(!q.empty()) {
-            root = q.front();
-            q.pop();
-            count++;
-
-            if(root->left) q.push(root->left);
-            if(root->right) q.push(root->right);
-
-            if(count == levelCount) {
-                result.push_back(root->val);
-                levelCount = q.size();
-                count = 0;
-            }
-        }
-        
         return result;
+    }
+private:
+    void preorder(TreeNode *root, int level) {
+        if(!root) return;
+        level++;
+        if(!levelSet.count(level)) {
+            result.push_back(root->val);
+            levelSet.insert(level);
+        }
+        preorder(root->right, level);
+        preorder(root->left, level);
     }
 };
