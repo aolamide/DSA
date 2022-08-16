9c80d07e298e4867ac64c9116e5411e5d79c15b5
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-08-16 22:42:53 +0100

    Time: 399 ms (44.08%), Space: 113.4 MB (65.54%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int pos = 2;
+        int firstPoint = 0;
+        int lastPoint = 0;
+        int minDistance = INT_MAX;
+        ListNode *prev = head;
+        ListNode *curr = head->next;
+        
+        while(curr->next) {
+            int val = curr->val;
+            
+            //critical point
+            if((prev->val > val && curr->next->val > val) || (prev->val < val && curr->next->val < val)) {
+                if(!firstPoint) {
+                    firstPoint = pos;
+                } else {
+                    minDistance = min(minDistance, pos - lastPoint);
+                }
+                lastPoint = pos;
+            }
+            prev = curr;
+            curr = curr->next;
+            pos++;
+        }
+        
+        if(!firstPoint || firstPoint == lastPoint) return { -1, -1 };
+        
+        return { minDistance, lastPoint - firstPoint };
+    }
+};
+
+//traverse list
+//keep track of position of each node
+//keep track of previous node
+//compare previous node and next node to current node
+//store if it is a critical point
+//max distance if distance between first and last critical points
+//keep track of previous critical point and use the position of next critical point to determine min distance.
+//keep updating min distance
