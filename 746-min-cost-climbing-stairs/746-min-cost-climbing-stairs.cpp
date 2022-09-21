09e42ab02e5b8c7d3254c372fb79d07642e03917
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-09-21 16:59:20 +0100

    Time: 16 ms (25.53%), Space: 13.5 MB (99.90%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,25 +1,44 @@
+// class Solution {
+//     vector<int> c;
+//     vector<int> dp;
+// private:
+//     int minCost(int i) {
+//         if(i < 2) return c[i];
+        
+//         if(dp[i]) return dp[i];
+        
+//         int mc = c[i] + min(minCost(i - 1), minCost(i - 2));
+        
+//         dp[i] = mc;
+        
+//         return mc;
+//     }
+// public:
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         cost.push_back(0);
+//         c = cost;
+        
+//         dp = vector<int>(cost.size(), 0);
+        
+//         return minCost(cost.size() - 1); 
+//     }
+// };
+
+//optimised
 class Solution {
-    vector<int> c;
-    vector<int> dp;
-private:
-    int minCost(int i) {
-        if(i < 2) return c[i];
-        
-        if(dp[i]) return dp[i];
-        
-        int mc = c[i] + min(minCost(i - 1), minCost(i - 2));
-        
-        dp[i] = mc;
-        
-        return mc;
-    }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        cost.push_back(0);
-        c = cost;
+        int prev1 = cost[1];
+        int prev2 = cost[0];
         
-        dp = vector<int>(cost.size(), 0);
+        for(int i = 2; i < cost.size(); i++) {
+            int tmp = prev1;
+            
+            prev1 = cost[i] + min(prev1, prev2);
+            
+            prev2 = tmp;
+        }
         
-        return minCost(cost.size() - 1); 
+        return min(prev1, prev2);
     }
 };
