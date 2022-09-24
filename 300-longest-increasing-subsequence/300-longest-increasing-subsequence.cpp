acbe2f789d1a584fbfc494e68aa15dc692172fc0
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-09-24 23:54:35 +0100

    Time: 714 ms (19.22%), Space: 10.4 MB (67.25%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,31 +1,44 @@
+// class Solution {
+// public:
+//     int recurse(vector<int>& nums, vector<vector<int>>& dp, int currIndex, int valIndex) {
+//         if(currIndex < 0) return 0;
+        
+//         if(dp[currIndex][valIndex] > -1) return dp[currIndex][valIndex];
+        
+//         int recurseSameValue = recurse(nums, dp, currIndex - 1, valIndex);
+        
+//         if(nums[currIndex] >= nums[valIndex]) {
+//             dp[currIndex][valIndex] = recurseSameValue;
+//         } else {
+//             dp[currIndex][valIndex] = max(1 + recurse(nums, dp, currIndex - 1, currIndex), recurseSameValue);
+//         }
+        
+//         return dp[currIndex][valIndex];
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+
+//         int result = 1;
+//         vector<vector<int>> dp = vector<vector<int>>(n, vector<int>(n, -1));
+        
+//         for(int i = nums.size() - 1; i >= 0; i--) {
+//             result = max(result, 1 + recurse(nums, dp, i - 1, i));
+//         }
+        
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
-    int recurse(vector<int>& nums, vector<vector<int>>& dp, int currIndex, int valIndex) {
-        if(currIndex < 0) return 0;
-        
-        if(dp[currIndex][valIndex] > -1) return dp[currIndex][valIndex];
-        
-        int recurseSameValue = recurse(nums, dp, currIndex - 1, valIndex);
-        
-        if(nums[currIndex] >= nums[valIndex]) {
-            dp[currIndex][valIndex] = recurseSameValue;
-        } else {
-            dp[currIndex][valIndex] = max(1 + recurse(nums, dp, currIndex - 1, currIndex), recurseSameValue);
-        }
-        
-        return dp[currIndex][valIndex];
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-
-        int result = 1;
-        vector<vector<int>> dp = vector<vector<int>>(n, vector<int>(n, -1));
-        
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            result = max(result, 1 + recurse(nums, dp, i - 1, i));
-        }
-        
-        return result;
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        return *max_element(dp.begin(), dp.end());
     }
 };
