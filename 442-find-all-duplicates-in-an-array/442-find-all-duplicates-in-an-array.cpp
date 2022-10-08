fd451816e49d7373d7a4ae3922b84f8cf7680a1a
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-08 20:27:18 +0100

    Time: 174 ms (23.39%), Space: 33.5 MB (93.74%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -5,17 +5,12 @@ public:
         int n = nums.size();
         
         for (int i = 0; i < n;i++) {
-            // for(int a : nums) cout << a << " ";
-            // cout << endl;
             int num = nums[i] % n;;
             
             if (num == 0) num = n;
 
             if (nums[num-1] > n) res.push_back(num);
             else nums[num-1] += n;
-            
-            
-            
         }
         
         return res;
