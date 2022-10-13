2f419047b62519353f098b9be35460f909706627
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-13 22:42:40 +0100

    Time: 582 ms (90.49%), Space: 88.9 MB (82.88%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<bool> seen(intervals.size(), false);
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i = 0; i < intervals.size(); i++) {
+            if(!pq.empty() && pq.top() < intervals[i][0]) {
+                pq.pop();
+            }
+
+            pq.push(intervals[i][1]);
+        }
+
+        return pq.size();
+
+    }
+};
