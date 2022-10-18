b7088bb80a18ff60fdc0c736723f3a7a63ecd1b7
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-18 23:00:28 +0100

    Time: 0 ms (100.00%), Space: 7.7 MB (77.45%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,53 @@
+//recursive solution TLE
+// class Solution {
+// public:
+//     int rec(vector<int>& nums, int i) {
+//         if(i < 0) return 0;
+//         if(i == 0) return nums[0];
+//         if(i == 1) return nums[1];
+
+//         return max(rec(nums, i - 2)  + nums[i], rec(nums, i - 3)  + nums[i]);
+//     }
+//     int rob(vector<int>& nums) {
+//         return max(rec(nums, nums.size() - 1), rec(nums, nums.size() - 2));
+//     }
+// };
+
+//recursive with DP
+// class Solution {
+// public:
+//     vector<int> dp;
+//     int rec(vector<int>& nums, int i) {
+//         if(i < 0) return 0;
+//         if(i <= 1) return nums[i];
+//         if(dp[i] > -1) return dp[i];
+        
+//         dp[i] = max(rec(nums, i - 2)  + nums[i], rec(nums, i - 3)  + nums[i]);
+        
+//         return dp[i];
+//     }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         dp = vector<int>(n, -1);
+//         return max(rec(nums, n - 1), rec(nums, n - 2));
+//     }
+// };
+
+//optimised DP
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int maxA = nums[0];
+        int maxB = nums[1];
+        
+        for(int i = 2; i < nums.size(); i++) {
+            int tmp = max(maxA, maxB);
+            maxB = max(maxB, nums[i] + maxA);
+            maxA = tmp;
+        }
+        
+        return max(maxA, maxB);
+    }
+};
