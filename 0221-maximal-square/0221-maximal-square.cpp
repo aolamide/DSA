d1799face05e81dec6903e55a2ba811d6b721007
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-24 20:23:02 +0100

    Time: 242 ms (22.50%), Space: 20.9 MB (48.09%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,43 +4,21 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        int result = 0;
+        int maxL = 0;
+    
+        vector<int> dp(n + 1, 0);
         
-//         for(int i = m - 1; i >= 0; i--) {
-//             for(int j = n - 1; j >= 0; j--) {
-//                 if(matrix[i][j] == '0') {
-//                     dp[i][j] = 0;
-//                 } else {
-//                     dp[i][j] = (matrix[i][j] - '0') + min(min(dp[i + 1][j], dp[i][j + 1]), dp[i + 1][j + 1]);
-//                     result = max(result, dp[i][j]);
-//                 }
-                
-//             }
-//         }
-                vector<int> dp(n + 1, 0);
         for(int i = m - 1; i >= 0; i--) {
             vector<int> inner(n + 1, 0);
             for(int j = n - 1; j >= 0; j--) {
-                if(matrix[i][j] == '0') {
-                    dp[j] = 0;
-                } else {
+                if(matrix[i][j] != '0') {
                     inner[j] = (matrix[i][j] - '0') + min(min(dp[j], inner[j + 1]), dp[j + 1]);
-                    result = max(result, inner[j]);
+                    maxL = max(maxL, inner[j]);
                 }
-                
             }
             dp = inner;
         }
         
-        
-        // for(int i = 0; i < m; i++) {
-        //     for(int j = 0; j < n; j++) {
-        //         cout << dp[i][j] << ' ';
-        //     }
-        //     cout << endl;
-        // }
-        
-        return result * result;
+        return maxL * maxL;
     }
 };
