fd00ad17ef929fe839df0d60874b179b4fed4b8d
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-24 20:20:59 +0100

    Time: 213 ms (36.87%), Space: 20.8 MB (71.37%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,32 +4,34 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         int result = 0;
         
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
-                if(matrix[i][j] == '0') {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = (matrix[i][j] - '0') + min(min(dp[i + 1][j], dp[i][j + 1]), dp[i + 1][j + 1]);
-                    result = max(result, dp[i][j]);
-                }
-                
-            }
-        }
-//                 vector<int> dp(n + 1, 0);
 //         for(int i = m - 1; i >= 0; i--) {
 //             for(int j = n - 1; j >= 0; j--) {
 //                 if(matrix[i][j] == '0') {
-//                     dp[j] = 0;
+//                     dp[i][j] = 0;
 //                 } else {
-//                     dp[j] = (matrix[i][j] - '0') + min(min(dp[j], dp[i]), dp[j + 1]);
-//                     result = max(result, dp[j]);
+//                     dp[i][j] = (matrix[i][j] - '0') + min(min(dp[i + 1][j], dp[i][j + 1]), dp[i + 1][j + 1]);
+//                     result = max(result, dp[i][j]);
 //                 }
                 
 //             }
 //         }
+                vector<int> dp(n + 1, 0);
+        for(int i = m - 1; i >= 0; i--) {
+            vector<int> inner(n + 1, 0);
+            for(int j = n - 1; j >= 0; j--) {
+                if(matrix[i][j] == '0') {
+                    dp[j] = 0;
+                } else {
+                    inner[j] = (matrix[i][j] - '0') + min(min(dp[j], inner[j + 1]), dp[j + 1]);
+                    result = max(result, inner[j]);
+                }
+                
+            }
+            dp = inner;
+        }
         
         
         // for(int i = 0; i < m; i++) {
