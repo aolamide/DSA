2323d48619be65efe23c2ae6a543ffbf2a3bdba9
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-24 20:29:48 +0100

    Time: 231 ms (27.85%), Space: 20.9 MB (66.70%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -7,16 +7,21 @@ public:
         int maxL = 0;
     
         vector<int> dp(n + 1, 0);
+        int last = 0;
         
         for(int i = m - 1; i >= 0; i--) {
             vector<int> inner(n + 1, 0);
             for(int j = n - 1; j >= 0; j--) {
+                int tmp = dp[j];
                 if(matrix[i][j] != '0') {
-                    inner[j] = (matrix[i][j] - '0') + min(min(dp[j], inner[j + 1]), dp[j + 1]);
-                    maxL = max(maxL, inner[j]);
+                    dp[j] = 1 + min(min(dp[j], last), dp[j + 1]);
+                    maxL = max(maxL, dp[j]);
+                } else {
+                    dp[j] = 0;
                 }
+                last = tmp;
             }
-            dp = inner;
+            
         }
         
         return maxL * maxL;
