3b05f46e2b735f327ed796f31989a5e20e1e5a10
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-31 22:48:31 +0100

    Time: 52 ms (5.93%), Space: 17.6 MB (15.29%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+     for(int r = 1; r < matrix.size(); r++) {
+         for(int c = 1; c < matrix[0].size(); c++) {
+             if(matrix[r][c] != matrix[r - 1][c - 1]) return false;
+         }
+     }   
+    return true;
+    
+    }
+};
