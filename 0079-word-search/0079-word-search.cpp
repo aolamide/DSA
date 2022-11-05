66601369d375ac705dbd2005ebeb473084c077a2
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-11-06 00:05:25 +0100

    Time: 740 ms (47.72%), Space: 8 MB (79.49%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     
-    bool backtrack(vector<vector<char>>& board, int row, int col, string& word, int wordIdx, vector<vector<bool>>& seen) {
+    bool backtrack(vector<vector<char>>& board, int row, int col, string& word, int wordIdx) {
         if(wordIdx == word.size()) {
             return true;
         }
         
-        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[wordIdx] || seen[row][col]) {
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[wordIdx] || board[row][col] == '*') {
             return false;
         }
         
-        seen[row][col] = true;
+        char original = board[row][col];
+        
+        board[row][col] = '*';
         
         for(int i = 0; i < 4; i++) {
             int nextRow = row + dirs[i][0];
             int nextCol = col + dirs[i][1];
             
-            if(backtrack(board, nextRow, nextCol, word, wordIdx + 1, seen)) {
+            if(backtrack(board, nextRow, nextCol, word, wordIdx + 1)) {
                 return true;
             }
         }
-        seen[row][col] = false;
+        board[row][col] = original;
         
         return false;
         
@@ -29,12 +31,11 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-        vector<vector<bool>> seen(m, vector<bool>(n, false));
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(board[i][j] == word[0]) {
-                    if(backtrack(board, i, j, word, 0, seen)) return true;
+                    if(backtrack(board, i, j, word, 0)) return true;
                 }
             }
         }
