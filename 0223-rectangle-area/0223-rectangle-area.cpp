698634457a63671aab1c0bdebd3c0c2e39210683
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-11-17 18:54:33 +0100

    Time: 16 ms (51.22%), Space: 5.9 MB (32.00%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1);
+        int ox_start = max(ax1, bx1);
+        int ox_end = min(ax2, bx2);
+        
+        int oy_start = max(ay1, by1);
+        int oy_end = min(ay2, by2);
+        
+        if(ox_start > ax2 || ox_start > bx2) return area;
+        if(ox_end < ax1 || ox_end < bx1) return area;
+        
+        if(oy_start > by2 || oy_start > by2) return area;
+        if(oy_end < by1 || oy_end < by1) return area;
+        
+        int overlap_area = (ox_end - ox_start) * (oy_end - oy_start);
+        
+        return area - overlap_area;
+    }
+};
