1b5ddd458d657d5be768b88a2aad99bcfc1a9695
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-11-18 16:12:24 +0100

    Time: 20 ms (98.09%), Space: 13.9 MB (50.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -11,12 +11,20 @@ public:
         int px = find(x);
         int py = find(y);
         
-        parents[py] = px;
+        if(rank[px] > rank[py]) {
+            parents[py] = px;
+        } else if(rank[px] < rank[py]) {
+            parents[px] = py;
+        } else {
+            parents[py] = px;
+            rank[py]++;
+        }
     };
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         parents = vector<int>(n);
+        rank = vector<int>(n, 1);
         iota(parents.begin(), parents.end(), 0);
         
         for(int i = 0; i < n; i++) {
