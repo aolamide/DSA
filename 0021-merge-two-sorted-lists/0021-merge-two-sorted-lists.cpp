43891808135e697e2c994af01cc1e1bb46b1cb42
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-15 13:10:35 +0100

    Time: 6 ms (92.60%), Space: 14.8 MB (43.41%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void rec(ListNode* list1, ListNode* list2, ListNode* curr) {
+        if(!list1) {
+            curr->next = list2;
+            return;
+        }
+        if(!list2){
+            curr->next = list1;
+            return;
+        }
+        if(list1->val < list2->val) {
+            curr->next = list1;
+            list1 = list1->next;
+        } else {
+            curr->next = list2;
+            list2 = list2->next;
+        }
+        curr = curr->next;
+        
+        rec(list1, list2, curr);
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) return list2;
+        if(!list2) return list1;
+        
+        ListNode *sentinel, *curr;
+        sentinel = new ListNode();
+        curr = sentinel;
+        
+        rec(list1, list2, sentinel);
+        
+//         while(list1 && list2) {
+//             // cout << list1->val << " " << list2->val << endl;
+//             if(list1->val < list2->val) {
+//                 curr->next = list1;
+//                 list1 = list1->next;
+//             } else {
+//                 curr->next = list2;
+//                 list2 = list2->next;
+//             }
+//              curr = curr->next;
+//         }
+        
+//         if(list1) curr->next = list1;
+//         if(list2) curr->next = list2;
+        
+        return sentinel->next;
+    }
+};
+
+// thought process
+// init sentinel and set curr as sentinel
+//loop through both lists, append l1 to curr if it lower and l2 to curr if l2 is lower.
+//move curr to curr->next;
+//append whichever list still has values to curr
+//return sentinel->next
