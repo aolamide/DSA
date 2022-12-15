afbdf3a27a89eea5c7fbf418ccfd2463ba035ae3
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-15 12:04:12 +0100

    Time: 468 ms (80.16%), Space: 174.2 MB (29.85%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,60 @@
+// class LRUCache {
+// public:
+//     LRUCache(int capacity) {
+        
+//     }
+    
+//     int get(int key) {
+        
+//     }
+    
+//     void put(int key, int value) {
+        
+//     }
+// };
+class LRUCache {
+list<pair<int, int>> dll;
+unordered_map<int, list<pair<int, int>>::iterator> m;
+int capacity;
+public:
+    LRUCache(int _capacity) {
+        capacity = _capacity;
+    }
+
+    int get(int key) {
+        if(m.count(key) == 0) return -1;
+        auto itr = m[key];
+        auto item = *itr;
+        dll.erase(itr);
+        dll.push_front(item);
+        m[key] = dll.begin();
+        return item.second;
+    }
+
+    void put(int key, int value) {
+        if(m.count(key) == 1) {
+            // If key exists, update value for that key
+            auto itr = m[key];
+            dll.erase(itr);
+            dll.push_front({key, value});
+            m[key] = dll.begin();
+        } else {
+            if(dll.size() >= capacity) {
+                // Evict LRU
+                auto item = dll.back();
+                dll.pop_back();
+                m.erase(item.first);
+            }
+            // Insert new key, val
+            dll.push_front({key, value});
+            m[key] = dll.begin();
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
