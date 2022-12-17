bb8d343c34bfcf66d0f02026fffd88d6822f5c3a
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-17 21:53:18 +0100

    Time: 5 ms (15.76%), Space: 6.3 MB (52.76%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int openings = 0;
+        int depth = 0;
+        
+        int answer = 0;
+        
+        for(char ch : s) {
+            if(ch == '(') depth++;
+            else if(ch == ')') depth--;
+            answer = max(answer, depth);
+        }
+        
+        return answer;
+    }
+};
