3dbafe96db09714f5d20bd1e528ff9ec0d70d5dc
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-22 23:12:50 +0100

    Time: 815 ms (13.04%), Space: 283 MB (10.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,62 @@
 class Solution {
 public:
-    int palindromeSearch(string s, int l, int r) {
-        while(l >= 0 && r < s.size() && s[l] == s[r]) {
-            l--;
-            r++;
+//     int palindromeSearch(string s, int l, int r) {
+//         while(l >= 0 && r < s.size() && s[l] == s[r]) {
+//             l--;
+//             r++;
+//         }
+//         return r - l - 1;
+//     }
+//     string longestPalindrome(string s) {
+//         int l = 0, r = 0;
+        
+//         for(int i = 0; i < s.size(); i++) {
+//             int l1 = palindromeSearch(s, i, i);
+//             int l2 = palindromeSearch(s, i, i + 1);
+            
+//             int size = max(l2, l1);
+            
+//             if(size > r - l + 1) {
+//                 l = i - (size - 1) / 2;
+//                 r = i + size/2;
+//             }
+//         }
+
+//         return s.substr(l, r - l + 1);
+//     }
+    vector<vector<int>> dp;
+    int rec(int start, int end, string& s) {
+        if(start > end) return true;
+        
+        if(dp[start][end] > -1) return dp[start][end];
+        
+        if(start == end) dp[start][end] = 1;
+        
+        else {
+            dp[start][end] = (s[start] == s[end]) && rec(start + 1, end - 1, s);
         }
-        return r - l - 1;
+
+        return dp[start][end];
+        
     }
     string longestPalindrome(string s) {
+        dp = vector<vector<int>>(s.size(), vector<int>(s.size(), -1));
+        
         int l = 0, r = 0;
         
         for(int i = 0; i < s.size(); i++) {
-            int l1 = palindromeSearch(s, i, i);
-            int l2 = palindromeSearch(s, i, i + 1);
-            
-            int size = max(l2, l1);
-            
-            if(size > r - l + 1) {
-                l = i - (size - 1) / 2;
-                r = i + size/2;
+            for(int j = s.size() - 1; j >= i; j--) {
+                if(rec(i, j, s) == 1) {
+                    int size = j - i + 1;
+                    
+                    if(size > r - l + 1) {
+                        l = i;
+                        r = j;
+                    }
+                }
             }
         }
-
+        
         return s.substr(l, r - l + 1);
     }
 };
