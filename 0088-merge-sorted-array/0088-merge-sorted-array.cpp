1d7f3f5ab2ffc52db77e14156138cb85128c54d2
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-25 22:00:31 +0100

    Time: 3 ms (78.62%), Space: 9.1 MB (73.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> result;
-        int p1 = 0, p2 = 0;
+//         vector<int> result;
+//         int p1 = 0, p2 = 0;
         
-        while(p1 < m && p2 < n) {
-            if(nums1[p1] < nums2[p2]) {
-                result.push_back(nums1[p1]);
-                p1++;
+//         while(p1 < m && p2 < n) {
+//             if(nums1[p1] < nums2[p2]) {
+//                 result.push_back(nums1[p1]);
+//                 p1++;
+//             } else {
+//                 result.push_back(nums2[p2]);
+//                 p2++;
+//             }
+//         }
+        
+//         while(p1 < m) {
+//             result.push_back(nums1[p1]);
+//             p1++;
+//         }
+//         while(p2 < n) {
+//             result.push_back(nums2[p2]);
+//             p2++;
+//         }
+        
+//         nums1 = result;
+        
+        int k = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        
+        while(p1 >= 0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[k] = nums1[p1];
+                p1--;
             } else {
-                result.push_back(nums2[p2]);
-                p2++;
+                nums1[k] = nums2[p2];
+                p2--;
             }
+            k--;
         }
         
-        while(p1 < m) {
-            result.push_back(nums1[p1]);
-            p1++;
-        }
-        while(p2 < n) {
-            result.push_back(nums2[p2]);
-            p2++;
+        while(p2 >= 0) {
+            nums1[k] = nums2[p2];
+            p2--;
+            k--;
         }
         
-        nums1 = result;
     }
 };
