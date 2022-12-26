969e6af52f488012b099825604ef6921ba45ad1c
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-12-26 15:19:11 +0100

    Time: 63 ms (95.48%), Space: 48.3 MB (94.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        if(nums.size() == 1) return true;
+        int last = nums[0];
         
-        for(int i = 1; i < dp.size(); i++) {
-            if(dp[i - 1] == 0) return false;
-            dp[i] = max(nums[i], dp[i - 1] - 1);
+        for(int i = 1; i < nums.size(); i++) {
+            if(last == 0) return false;
+            last = max(nums[i], last - 1);
         }
         
         return true;
