3b54f50dc13cba17e581e504f47ec25793c24884
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-01-05 22:35:50 +0100

    Time: 520 ms (46.41%), Space: 173.8 MB (47.31%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,57 +1,120 @@
 // class LRUCache {
+// list<pair<int, int>> dll;
+// unordered_map<int, list<pair<int, int>>::iterator> m;
+// int capacity;
 // public:
-//     LRUCache(int capacity) {
-        
+//     LRUCache(int _capacity) {
+//         capacity = _capacity;
 //     }
-    
+
 //     int get(int key) {
-        
+//         if(m.count(key) == 0) return -1;
+//         auto itr = m[key];
+//         auto item = *itr;
+//         dll.erase(itr);
+//         dll.push_front(item);
+//         m[key] = dll.begin();
+//         return item.second;
 //     }
-    
+
 //     void put(int key, int value) {
-        
+//         if(m.count(key) == 1) {
+//             // If key exists, update value for that key
+//             auto itr = m[key];
+//             dll.erase(itr);
+//             dll.push_front({key, value});
+//             m[key] = dll.begin();
+//         } else {
+//             if(dll.size() >= capacity) {
+//                 // Evict LRU
+//                 auto item = dll.back();
+//                 dll.pop_back();
+//                 m.erase(item.first);
+//             }
+//             // Insert new key, val
+//             dll.push_front({key, value});
+//             m[key] = dll.begin();
+//         }
 //     }
 // };
-class LRUCache {
-list<pair<int, int>> dll;
-unordered_map<int, list<pair<int, int>>::iterator> m;
-int capacity;
+class Node {
 public:
-    LRUCache(int _capacity) {
-        capacity = _capacity;
-    }
-
-    int get(int key) {
-        if(m.count(key) == 0) return -1;
-        auto itr = m[key];
-        auto item = *itr;
-        dll.erase(itr);
-        dll.push_front(item);
-        m[key] = dll.begin();
-        return item.second;
-    }
-
-    void put(int key, int value) {
-        if(m.count(key) == 1) {
-            // If key exists, update value for that key
-            auto itr = m[key];
-            dll.erase(itr);
-            dll.push_front({key, value});
-            m[key] = dll.begin();
-        } else {
-            if(dll.size() >= capacity) {
-                // Evict LRU
-                auto item = dll.back();
-                dll.pop_back();
-                m.erase(item.first);
-            }
-            // Insert new key, val
-            dll.push_front({key, value});
-            m[key] = dll.begin();
-        }
+    Node *prev, *next;
+    pair<int, int> val;
+    Node(pair<int, int> value) {
+        val = value;
+        prev = NULL;
+        next = NULL;
     }
 };
 
+class LRUCache {
+private:
+int capacity;
+int currSize = 0; 
+Node *start = new Node({-1,-1}), *end = new Node({-1, -1});
+unordered_map<int, Node*> mp;
+    
+public:
+    LRUCache(int _capacity) {
+        capacity = _capacity;
+        start->next = end;
+        end->prev = start;
+    }
+    void addNode(Node* newNode) {
+        newNode->next = end;
+        newNode->prev = end->prev;
+        end->prev->next = newNode;
+        end->prev = newNode;
+        mp[newNode->val.first] = newNode;
+    }
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        mp.erase(node->val.first);
+    }
+    int get(int key) {
+        if(!mp.count(key)) return -1;
+        Node *curr = mp[key];
+        removeNode(curr);
+        addNode(curr);
+        return curr->val.second;
+    }
+
+    void put(int key, int value) {
+        if(mp.count(key)) {
+            Node *curr = mp[key];
+            removeNode(curr);
+            curr->val.second = value;
+            addNode(curr);
+            
+        } else {
+            if(currSize == capacity) {
+                //remove first node
+                removeNode(start->next);
+                currSize--;
+            }
+            
+            //insert node
+            Node *newNode = new Node({ key, value });
+            addNode(newNode);
+            currSize++;
+        }
+        
+        // Node *root = start;
+        // while(root) {
+        //     cout << root->val.first << "-" << root->val.second << "   ";
+        //     root = root->next;
+        // }
+        // cout << endl;
+    }
+};
+
+// ["LRUCache","put","put","get","put","put","get"]
+// [[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
+
+// ["LRUCache","put","put","put","put","get","get"]
+// [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
