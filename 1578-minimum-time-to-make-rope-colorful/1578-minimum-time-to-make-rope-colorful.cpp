8c042802039a7781fe20a753886df08f0833f1fa
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-01-06 23:34:49 +0100

    Time: 159 ms (89.56%), Space: 95.4 MB (50.10%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -5,18 +5,18 @@ public:
         
         int maxColor = neededTime[0];
         int runningSum = neededTime[0];
-        int colorCount = 1;
+        // int colorCount = 1;
         
         for(int i = 1; i < colors.size(); i++) {
             if(colors[i] == colors[i - 1]) {
                 maxColor = max(maxColor, neededTime[i]);
                 runningSum += neededTime[i];
-                colorCount++;
+                // colorCount++;
             } else {
                 result += runningSum - maxColor;
                 runningSum = neededTime[i];
                 maxColor = neededTime[i];
-                colorCount = 1;
+                // colorCount = 1;
             }
         }
         
