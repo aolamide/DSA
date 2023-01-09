7b24affa36edc414999c376e61dcb49d88775754
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-01-10 00:44:52 +0100

    Time: 11 ms (99.75%), Space: 16.9 MB (70.25%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -16,27 +16,47 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         if(!root) return root;
+//         queue<Node*> q;
+        
+//         q.push(root);
+//         int level = 1;
+        
+//         while(!q.empty()) {
+//             Node* curr = q.front();
+//             q.pop();
+//             if(curr->left) q.push(curr->left);
+//             if(curr->right) q.push(curr->right);
+            
+//             level--;
+//             if(level == 0) {
+//                 level = q.size();
+//             } else {
+//                 curr->next = q.front();
+//             }
+//         }
+        
+//         return root;
+//     }
+// };
+
 class Solution {
 public:
     Node* connect(Node* root) {
         if(!root) return root;
-        queue<Node*> q;
         
-        q.push(root);
-        int level = 1;
-        
-        while(!q.empty()) {
-            Node* curr = q.front();
-            q.pop();
-            if(curr->left) q.push(curr->left);
-            if(curr->right) q.push(curr->right);
-            
-            level--;
-            if(level == 0) {
-                level = q.size();
-            } else {
-                curr->next = q.front();
+        Node* curr = root;
+        while(curr->left) {
+            Node* c = curr;
+            while(c) {
+                c->left->next = c->right;
+                if(c->next) c->right->next = c->next->left;
+                c = c->next;
             }
+            curr = curr->left;
         }
         
         return root;
