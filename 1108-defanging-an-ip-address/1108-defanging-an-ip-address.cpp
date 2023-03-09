811db4b5ddd2c082a6cb5456208c214b82dbadfd
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-03-09 21:51:32 +0100

    Time: 0 ms (100.00%), Space: 5.9 MB (91.84%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        for(int i = 0; i < address.size(); i++) {            
+            if(address[i] == '.') {
+                address.replace(i, 1, "[.]");
+                i += 2;
+            }
+        }
+
+        return address;
+    }
+};
