23f371a59710e7cb47e0534d5af6ac3542927814
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-07-27 19:26:27 +0100

    Time: 3 ms (36.33%), Space: 6.8 MB (51.56%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        unordered_map<char, int> letterMap = {
+            {'q', 0}, {'w', 0}, {'e', 0}, {'r', 0}, {'t', 0}, {'y', 0}, {'u', 0}, {'i', 0}, {'o', 0}, {'p', 0},
+            {'a', 1}, {'s', 1}, {'d', 1}, {'f', 1}, {'g', 1}, {'h', 1}, {'j', 1}, {'k', 1}, {'l', 1},
+            {'z', 2}, {'x', 2}, {'c', 2}, {'v', 2}, {'b', 2}, {'n', 2}, {'m', 2}
+        };
+        vector<string> result;
+        
+        for(string word : words) {
+            int pos = letterMap[tolower(word[0])];
+            bool valid = true;
+            for(int i = 1; i < word.size(); i++) {
+                if(letterMap[tolower(word[i])] != pos) {
+                    valid = false;
+                    break;
+                };
+            }
+            if(valid) result.push_back(word);
+        }
+        
+        return result;
+    }
+};
