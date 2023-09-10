bfd0cd0c94808db34dd6d42ebcf7fca54a7a75d5
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-09-10 19:52:12 +0100

    Time: 2 ms (56.67%), Space: 6 MB (43.33%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long ans = 1;
+        long mod = 1e9 + 7;
+        
+        for(int i = 1; i <= n; i++) {
+            ans = ans * (i * 2 - 1) * i % mod;
+        }
+        
+        return ans;
+    }
+};
