fbbb70586ece1cbff34291dc439ba31ff20545f0
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-09-19 22:35:36 +0100

    Time: 86 ms (60.72%), Space: 61.5 MB (28.98%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //Linked list cycle detection algo
+        
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = 0;
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
