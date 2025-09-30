9d926ae5933951ca3688f52c8927426a826b7e86
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2025-10-01 00:08:41 +0100

    Time: 72 ms (69.35%), Space: 172.8 MB (95.55%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,102 +1,69 @@
-// class LRUCache {
-// list<pair<int, int>> dll;
-// unordered_map<int, list<pair<int, int>>::iterator> m;
-// int capacity;
-// public:
-//     LRUCache(int _capacity) {
-//         capacity = _capacity;
-//     }
+struct Node {
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
 
-//     int get(int key) {
-//         if(m.count(key) == 0) return -1;
-//         auto itr = m[key];
-//         auto item = *itr;
-//         dll.erase(itr);
-//         dll.push_front(item);
-//         m[key] = dll.begin();
-//         return item.second;
-//     }
-
-//     void put(int key, int value) {
-//         if(m.count(key) == 1) {
-//             // If key exists, update value for that key
-//             auto itr = m[key];
-//             dll.erase(itr);
-//             dll.push_front({key, value});
-//             m[key] = dll.begin();
-//         } else {
-//             if(dll.size() >= capacity) {
-//                 // Evict LRU
-//                 auto item = dll.back();
-//                 dll.pop_back();
-//                 m.erase(item.first);
-//             }
-//             // Insert new key, val
-//             dll.push_front({key, value});
-//             m[key] = dll.begin();
-//         }
-//     }
-// };
-class Node {
-public:
-    Node *prev, *next;
-    pair<int, int> val;
-    Node(pair<int, int> value) {
-        val = value;
-        prev = NULL;
-        next = NULL;
-    }
+    Node(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
 };
 
 class LRUCache {
 private:
-int capacity;
-int currSize = 0; 
-Node *start = new Node({-1,-1}), *end = new Node({-1, -1});
-unordered_map<int, Node*> mp;
-    
-public:
-    LRUCache(int _capacity) {
-        capacity = _capacity;
-        start->next = end;
-        end->prev = start;
-    }
-    void addNode(Node* newNode) {
-        newNode->next = end;
-        newNode->prev = end->prev;
-        end->prev->next = newNode;
-        end->prev = newNode;
-        mp[newNode->val.first] = newNode;
-    }
+    unordered_map<int, Node*> cache;
+    int cap;
+    Node *head;
+    Node* tail;
+
     void removeNode(Node* node) {
         node->prev->next = node->next;
         node->next->prev = node->prev;
-        // mp.erase(node->val.first);
     }
-    int get(int key) {
-        if(!mp.count(key)) return -1;
-        Node *curr = mp[key];
-        removeNode(curr);
-        addNode(curr);
-        return curr->val.second;
+    void addNode(Node* node) {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
     }
 
+    void moveNodeToFront(Node* node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->next = head;
+    }
+    
+    int get(int key) {
+        if(!cache.count(key)) return -1;
+        Node* n = cache[key];
+        moveNodeToFront(n);
+        return n->val;
+    }
+    
     void put(int key, int value) {
-        if(mp.count(key)) {
-            Node *curr = mp[key];
-            removeNode(curr);
-            curr->val.second = value;
-            addNode(curr);
-            
+        //if key already exists
+        if(cache.count(key)) {
+            Node* n = cache[key];
+            n->val = value;
+            moveNodeToFront(n);
         } else {
-            if(currSize == capacity) {
-                mp.erase(start->next->val.first);
-                removeNode(start->next);
-                currSize--;
+            //check capacity
+            if(cache.size() >= cap) {
+                //remove last node
+                Node* lastNode = tail->prev;
+                removeNode(lastNode);
+                cache.erase(lastNode->key);
             }
-            Node *newNode = new Node({ key, value });
+            //new node
+            Node* newNode = new Node(key, value);
             addNode(newNode);
-            currSize++;
+            cache[key] = newNode;
         }
     }
 };
